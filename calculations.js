3d65d71eb81ebfb026b645e116e18780b48765e7
NataZakharenka <natallia.kirsanava@gmail.com>
2024-08-13 23:03:08 +0300

    HT 2.2.1 Fixed issue in function arguments(upd)

--- calculations.js
+++ calculations.js
@@ -31,7 +31,7 @@ function decrement() {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
 console.log (increment());
 console.log (decrement());
